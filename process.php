21a2732462df8387bd0f2dff90c705d0b37eddae
Jeff Walsh <jeffpwalsh@gmail.com>
2020-07-23 10:00:27 +0200

    php header mod

--- process.php
+++ process.php
@@ -11,5 +11,5 @@
     //send email
     mail($admin_email, "New Form Submission", $message . ' - ' . $phone, "From:" . $email);
     
-    header('Location: http://edgeledger.net/success.html');
+    header('Location: https://jeffpwalsh.github.io/financial-website/success.html');
   }
